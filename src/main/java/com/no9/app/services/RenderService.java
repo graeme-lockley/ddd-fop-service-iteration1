4444cf71afe14005f4371cc3e84f7a05b6cbf22e
Graeme Lockley <graemel@no9.co.za>
2013-09-22 08:02:44 +0200

    Added another method to the interface to deal with a source File object rather than an InputStream - not too happy with this but will rework later.

--- src/main/java/com/no9/app/services/RenderService.java
+++ src/main/java/com/no9/app/services/RenderService.java
@@ -1,8 +1,11 @@
 package com.no9.app.services;
 
+import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface RenderService {
     void toPDF(TemplateID templateID, InputStream source, OutputStream output) throws RenderException;
+
+    void toPDF(TemplateID templateID, File sourceFile, OutputStream output) throws RenderException;
 }
