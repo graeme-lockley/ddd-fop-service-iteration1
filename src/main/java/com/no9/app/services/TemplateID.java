091247915b97f67e589a89a97502662ebd1f2b86
Graeme Lockley <graemel@no9.co.za>
2013-09-22 08:00:21 +0200

    Added some body to the TemplateID - it is a value object and it contains a URI to refer to the template in question

--- src/main/java/com/no9/app/services/TemplateID.java
+++ src/main/java/com/no9/app/services/TemplateID.java
@@ -1,4 +1,13 @@
 package com.no9.app.services;
 
 public class TemplateID {
+    final private String templateURI;
+
+    public TemplateID(String templateURI) {
+        this.templateURI = templateURI;
+    }
+
+    public String getTemplateURI() {
+        return templateURI;
+    }
 }
