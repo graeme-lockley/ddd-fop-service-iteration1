ffcf8dd434236f576e915c3680e8821437612462
Graeme Lockley <graemel@no9.co.za>
2013-09-23 21:17:06 +0200

    Made use of the refactored out Resource class which deals with all of the behaviours associated with accessing a resource.

--- src/main/java/com/no9/app/ui/RetrievePDF.java
+++ src/main/java/com/no9/app/ui/RetrievePDF.java
@@ -4,17 +4,20 @@ import com.no9.app.adaptors.FOPRenderService;
 import com.no9.app.services.RenderException;
 import com.no9.app.services.RenderService;
 import com.no9.app.services.TemplateID;
+import com.no9.app.utils.Resource;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class RetrievePDF extends HttpServlet {
     private RenderService renderService;
 
-    private static final String XML_SOURCE_RESOURCE_NAME = "Hello.xml";
+    private static final Resource XML_SOURCE_RESOURCE = new Resource("Hello.xml");
     private static final TemplateID XSLT_TEMPLATE_ID = new TemplateID("/HelloWorld.xsl");
     private static final String RESULT_FILE_NAME = "HelloWorld.pdf";
 
@@ -23,17 +26,13 @@ public class RetrievePDF extends HttpServlet {
         resp.setContentType("application/pdf");
         resp.setHeader("Content-Disposition", "attachment; filename=\"" + RESULT_FILE_NAME + "\"");
 
-        try (InputStream inputStream = new FileInputStream(getResourceFile(XML_SOURCE_RESOURCE_NAME))) {
+        try (InputStream inputStream = XML_SOURCE_RESOURCE.toInputStream()) {
             getRenderService().toPDF(XSLT_TEMPLATE_ID, inputStream, resp.getOutputStream());
         } catch (FileNotFoundException|RenderException ex) {
             resp.setContentType("text/html");
             resp.getOutputStream().println("<h1>Exception</h1>");
             resp.getOutputStream().println(ex.getMessage());
         }
-    }
-
-    private File getResourceFile(String resourceName) {
-        return new File(RetrievePDF.class.getClassLoader().getResource(resourceName).getFile());
     }
 
     private RenderService getRenderService() {
