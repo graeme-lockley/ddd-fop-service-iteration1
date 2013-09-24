7ad09cd2a812b1458d15404e0ecc7f97cdd81333
Graeme Lockley <graemel@no9.co.za>
2013-09-24 18:05:34 +0200

    Added an equals and hasCode method so that instances of this class can be used as a key in a hash.

--- src/main/java/com/no9/app/utils/TemplateID.java
+++ src/main/java/com/no9/app/utils/TemplateID.java
@@ -10,4 +10,21 @@ public class TemplateID {
     public String getTemplateURI() {
         return templateURI;
     }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        return templateURI.equals(((TemplateID) object).templateURI);
+    }
+
+    @Override
+    public int hashCode() {
+        return templateURI.hashCode();
+    }
 }
